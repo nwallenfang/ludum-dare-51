b2807723bc792191862a381071903670403ab124
nheeb <nitai64@yahoo.de>
2022-10-01 19:13:52 +0200

    Bob

---
+++ Logic/Enemies/Bob.gd
@@ -0,0 +1,74 @@
+extends KinematicBody
+
+enum STATES {IDLE, AGGRO}
+var state = STATES.AGGRO
+
+var walk_speed := .2
+export var gravity_multiplier := 3.0
+export var speed := 10
+export var acceleration := 8
+export var deceleration := 10
+export(float, 0.0, 1.0, 0.05) var air_control := 0.3
+export var jump_height := 10
+var direction := Vector3()
+var input_axis := Vector2()
+var velocity := Vector3()
+var snap := Vector3()
+var up_direction := Vector3.UP
+var stop_on_slope := true
+onready var floor_max_angle: float = deg2rad(45.0)
+onready var gravity = (ProjectSettings.get_setting("physics/3d/default_gravity") * gravity_multiplier)
+
+func _ready():
+	pass
+
+func _physics_process(delta):
+	match state:
+		STATES.IDLE:
+			pass
+		STATES.AGGRO:
+			var direction_to_player := self.global_translation.direction_to(Game.player.global_translation)
+			direction_to_player.y = 0.0
+			var normalized_walk_direction := direction_to_player.normalized()
+			direction = normalized_walk_direction * walk_speed
+	
+	if is_on_floor():
+		snap = -get_floor_normal() - get_floor_velocity() * delta
+		
+		# Workaround for sliding down after jump on slope
+		if velocity.y < 0:
+			velocity.y = 0
+		
+	else:
+		# Workaround for 'vertical bump' when going off platform
+		if snap != Vector3.ZERO && velocity.y != 0:
+			velocity.y = 0
+		
+		snap = Vector3.ZERO
+		
+		velocity.y -= gravity * delta
+	
+	accelerate(delta)
+	
+	velocity = move_and_slide_with_snap(velocity, snap, up_direction, stop_on_slope, 4, floor_max_angle)
+
+func accelerate(delta: float) -> void:
+	# Using only the horizontal velocity, interpolate towards the input.
+	var temp_vel := velocity
+	temp_vel.y = 0
+	
+	var temp_accel: float
+	var target: Vector3 = direction * speed
+	
+	if direction.dot(temp_vel) > 0:
+		temp_accel = acceleration
+	else:
+		temp_accel = deceleration
+	
+	if not is_on_floor():
+		temp_accel *= air_control
+	
+	temp_vel = temp_vel.linear_interpolate(target, temp_accel * delta)
+	
+	velocity.x = temp_vel.x
+	velocity.z = temp_vel.z
