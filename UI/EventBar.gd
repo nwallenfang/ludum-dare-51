b6b71de4248514d8603c1e2695ad7331add66be9
nwallenfang <nils@wallenfang.de>
2022-10-01 21:33:16 +0200

    fix EVENT BAR ICONSSS

--- UI/EventBar.gd
+++ UI/EventBar.gd
@@ -27,7 +27,7 @@ func init_fixed_events():
 		new_icon.material = new_icon.material.duplicate()
 		new_icon.material.set_shader_param("texture_resource", fixed.icon)
 		new_icon.set("event_name", fixed.event_name)
-		new_icon.rect_position = get_node("%Icon" + str(fixed_number)).rect_position
+		new_icon.rect_position = get_node("%EventBar/Icon" + str(fixed_number)).rect_position
 		new_icon.rect_position.y = new_icon.rect_position.y - fixed_icon_translation
 		new_icon.modulate = greyed_out
 
@@ -46,7 +46,7 @@ func event_triggered(event):
 		return
 	
 	
-	var icon_node: ColorRect = get_node("%Icon" + str(Events.number_triggered))
+	var icon_node: ColorRect = get_node("%EventBar/Icon" + str(Events.number_triggered))
 	icon_node.material = icon_node.material.duplicate()
 	icon_node.material.set_shader_param("texture_resource", event.icon)
 	$TriggerParticles.position = icon_node.rect_position + 0.5 * icon_node.rect_size
