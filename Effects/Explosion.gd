9aa34f9e5b0e2120a5c4b2fa6b8bcdffe8d54713
nheeb <nitai64@yahoo.de>
2022-10-02 13:50:10 +0200

    Explosion vfx

--- Effects/Explosion.gd
+++ Effects/Explosion.gd
@@ -1,6 +1,11 @@
 extends Spatial
 
 func _ready():
+	$Particles.emitting = true
+	$Particles2.emitting = true
+	$Particles3.emitting = true
+	$Tween.interpolate_property($OmniLight, "light_energy", 10.0, 0.0, .8)
 	yield(get_tree().create_timer(.1),"timeout")
 	$Area.queue_free()
-	yield(get_tree().create_timer(1),"timeout")
+	yield(get_tree().create_timer(1.5),"timeout")
+	queue_free()
