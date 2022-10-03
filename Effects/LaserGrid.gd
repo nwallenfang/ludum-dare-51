5aadd2827ceb4866ba20ed48447c102466a6705e
RitzGit <marvin@wallenfang.de>
2022-10-03 21:05:47 +0200

    Laser sound

--- Effects/LaserGrid.gd
+++ Effects/LaserGrid.gd
@@ -5,4 +5,10 @@ export var speed := .5
 func _physics_process(delta):
 	self.global_translation.y += speed * delta
 	if Game.player.global_translation.y <= self.global_translation.y:
-		Game.world.restart_level()#Game.player.get_hurt()
+		Game.world.restart_level()
+
+	
+	if Game.player.global_translation.y - self.global_translation.y < 15:
+		var new_db = (Game.player.global_translation.y - self.global_translation.y) / 18 * -80
+		
+		$MenacingStream.set_volume_db(clamp(new_db,-80, -20))
