37cd6daf17951a00973904252b70feb700fce967
nwallenfang <nils@wallenfang.de>
2022-10-03 19:26:15 +0200

    add credit scene

--- Levels/Credits.gd
+++ Levels/Credits.gd
@@ -8,4 +8,16 @@ extends Spatial
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	Events.set_process(false)
+	Game.text_screen_ui.set_text("EXPERIMENTAL")
+	Ui.to_credits()
+	
+	$CreditAnimation.play("credits")
+	
+	yield($CreditAnimation, "animation_finished")
+	yield(get_tree().create_timer(1.5), "timeout")
+	get_tree().quit(0)
+
+
+func fade_out():
+	Ui.get_node("CreditsAnimation").play("fade_credits_out")
