876e8e20eff1c31d455d1429b6810b407fe7a9ca
RitzGit <marvin@wallenfang.de>
2022-10-09 19:59:36 +0200

    event pickup fix

--- Objects/EventPickup.gd
+++ Objects/EventPickup.gd
@@ -2,12 +2,13 @@ extends Spatial
 
 export var event_name: String
 var event
-var icon := "res://Assets/Sprites/banana.png"
+var icon: Texture
 
 func _ready():
 	event = Events.event_index[event_name]
+	icon = event.icon
 	
-	$HoverPickup.mesh.surface_get_material(0).set_shader_param("event_icon", load(icon))
+	$HoverPickup.mesh.surface_get_material(0).set_shader_param("texture_albedo", icon)
 
 func _on_PickupBox_area_entered(area):
 	Events.event_stack.append(event)
